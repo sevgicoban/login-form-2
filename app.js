3bd04417aad5df90d5b052bbc1b573273fc99a05
sevgicoban <kadinkazanan7@gmail.com>
2023-10-03 07:05:26 +0300

    login

---
+++ app.js
@@ -0,0 +1,13 @@
+function validate() {
+    let username = document.getElementById("username").
+    value;
+    let password = document.getElementById("password"). 
+    value;
+
+    if (username === 'Webdev' && password === 'learn')
+    {
+        alert( 'login successfuly!');
+    } else {
+        alert('Login failed');
+    }
+}
